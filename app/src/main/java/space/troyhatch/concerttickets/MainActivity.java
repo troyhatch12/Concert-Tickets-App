665e59418c4dd609273ba6152b5e49f35609d0ba
troyhatch12 <troyhatchard@gmail.com>
2018-01-24 13:55:54 -0700

    First commit of concert tickets app

---
+++ app/src/main/java/space/troyhatch/concerttickets/MainActivity.java
@@ -0,0 +1,37 @@
+package space.troyhatch.concerttickets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    double costPerTicker = 79.99;
+    int numberOfTickets;
+    double totalCost;
+    String groupChoice;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText tickets = (EditText)findViewById(R.id.txtTickets);
+        Spinner group = (Spinner)findViewById(R.id.txtGroup);
+        Button cost = (Button)findViewById(R.id.btnCost);
+        cost.setOnClickListener(new View.OnClickListener() {
+            final TextView result = ((TextView)findViewById(R.id.txtResult))
+            @Override
+            public void onClick(View view) {
+                numberOfTickets = Integer.parseInt(tickets.getText().toString());
+                totalCost = costPerTicket * numberOfTickets;
+
+            }
+        });
+
+    }
+
+
+}
