4764d798d56a12d3fd9d605f93551de637bf69e6
troyhatch12 <troyhatchard@gmail.com>
2018-01-24 14:09:23 -0700

    Added button logic and display result

--- app/src/main/java/space/troyhatch/concerttickets/MainActivity.java
+++ app/src/main/java/space/troyhatch/concerttickets/MainActivity.java
@@ -8,8 +8,10 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
-    double costPerTicker = 79.99;
+    double costPerTicket = 79.99;
     int numberOfTickets;
     double totalCost;
     String groupChoice;
@@ -19,14 +21,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final EditText tickets = (EditText)findViewById(R.id.txtTickets);
-        Spinner group = (Spinner)findViewById(R.id.txtGroup);
+        final Spinner group = (Spinner)findViewById(R.id.txtGroup);
         Button cost = (Button)findViewById(R.id.btnCost);
         cost.setOnClickListener(new View.OnClickListener() {
-            final TextView result = ((TextView)findViewById(R.id.txtResult))
+            final TextView result = ((TextView)findViewById(R.id.txtResult));
             @Override
             public void onClick(View view) {
                 numberOfTickets = Integer.parseInt(tickets.getText().toString());
                 totalCost = costPerTicket * numberOfTickets;
+                DecimalFormat currency = new DecimalFormat("$###,###.##");
+                groupChoice = group.getSelectedItem().toString();
+                result.setText("Cost for" + groupChoice + "is " + currency.format(totalCost));
+
 
             }
         });
